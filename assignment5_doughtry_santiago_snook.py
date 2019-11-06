a89e669c7b38aed8863dc7f7fb6d30b7d9ad53e7
Samantha Santiago <samantha.e.santiago@gmail.com>
2019-11-06 18:16:47 -0500

    Upload initial file

---
+++ assignment5_doughtry_santiago_snook.py
@@ -0,0 +1,17 @@
+# Names: Brad, Caleb, Sam
+# Assignment #5
+
+#Dijkstra's Algorithm
+
+# nprime = []
+# for v in nodes:
+# if v != 9999
+# D(v) = c(u,v)
+# else D(v) = infinity
+
+#Repeat
+#find w not in nprime such that D(w) is min
+# add w to nprime
+#	D(v) = min(D(v), D(w)+c(w,v))
+# compare with if statement to determine min
+#until all of nodes in nprime
