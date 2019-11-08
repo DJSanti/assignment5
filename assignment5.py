26aa836898d6ca8b9b7d03c92c6cefb753db1e9e
Riddler6897 <bradforddoughty@gmail.com>
2019-11-08 09:07:21 -0600

    added getnum function and initial stuff

--- assignment5.py
+++ assignment5.py
@@ -5,6 +5,18 @@
 import sys
 import pandas as pd
 
+#getnum function
+def getnum(node, i):
+    print "{}, {}".format(node,i)
+    #Repeat
+    #while 
+    #find w not in nprime such that D(w) is min
+    # add w to nprime
+    #	D(v) = min(D(v), D(w)+c(w,v))
+    # compare with if statement to determine min
+    #until all of nodes in nprime
+    
+
 # get filename of .csv, get it as a dataframe
 filename = sys.argv[1]
 df = pd.read_csv(filename, index_col=0)
@@ -19,24 +31,22 @@ nprime = []
 node = raw_input("Please, provide the node's name: ")
 
 # get the row that corresponds to our node
-n = df.loc[[node]]
-print n
-print "\n"
+#n = df.loc[[node]]
 
 # for v in nodes:
 for col in df.columns:
 	# if v < 9999
 	if df.loc[node, col] < 9999:		
 		# D(v) = c(u,v)
-		dv = [node+col, df.loc[node,col]]
-		print dv
+		dv = [col, df.loc[node,col]]		
 		hold.append(dv)
+        nprime.append(col)
 	# else D(v) = infinity
 print hold
+print nprime
 
-#Repeat
-#find w not in nprime such that D(w) is min
-# add w to nprime
-#	D(v) = min(D(v), D(w)+c(w,v))
-# compare with if statement to determine min
-#until all of nodes in nprime
+for i in nprime:
+    if i != node:
+        getnum(node,i)
+    else:
+        n.append(node, 0)
