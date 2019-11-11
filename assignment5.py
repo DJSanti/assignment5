a2a1fee4e52475832070dfba3b4fb1902a3defe7
Riddler6897 <bradforddoughty@gmail.com>
2019-11-11 15:21:21 -0600

    Put in what Caleb worked on
    Still getting some errors and work in progress

--- assignment5.py
+++ assignment5.py
@@ -6,15 +6,9 @@ import sys
 import pandas as pd
 
 #getnum function
-def getnum(node, i):
-    print "{}, {}".format(node,i)
-    #Repeat
-    #while 
-    #find w not in nprime such that D(w) is min
-    # add w to nprime
-    #	D(v) = min(D(v), D(w)+c(w,v))
-    # compare with if statement to determine min
-    #until all of nodes in nprime
+def getnum(node, i, df):
+    #print "{}, {}".format(node,i)
+    return df.loc[node, i]
     
 
 # get filename of .csv, get it as a dataframe
@@ -42,11 +36,38 @@ for col in df.columns:
 		hold.append(dv)
         nprime.append(col)
 	# else D(v) = infinity
-print hold
+#print hold
+#print nprime
+
+n.append(node)
+nprime.remove(node)
+
+print "The following is nPrime"
 print nprime
 
-for i in nprime:
-    if i != node:
-        getnum(node,i)
-    else:
-        n.append(node, 0)
+distU = getnum(node, 'u', df)
+distV = getnum(node, 'v', df)
+distW = getnum(node, 'w', df)
+distX = getnum(node, 'x', df)
+distY = getnum(node, 'y', df)
+distZ = getnum(node, 'z', df)
+
+print "u: {}\nv: {}\nw: {}\nx: {}\ny: {}\nz: {}\n".format(distU, distV, distW, distX, distY, distZ)
+
+ #Repeat
+    #while 
+    #find w not in nprime such that D(w) is min
+    # add w to nprime
+    #	D(v) = min(D(v), D(w)+c(w,v))
+    # compare with if statement to determine min
+    #until all of nodes in nprime
+while ((len(nprime) > 0)):
+    for i in nprime:
+        shortest = ''
+        min = 9999
+        if(getnum(node, i, df) < min):
+            shortest = i
+    nprime.remove(shortest)
+    print nprime
+
+    #test = min(distU, (getnum(node,+ getnum(shortest, 'u', df)))
