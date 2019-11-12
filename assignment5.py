f18ff3f10e6a95bdb983e1d4e5db1114eb5e127a
Riddler6897 <bradforddoughty@gmail.com>
2019-11-12 14:49:43 -0600

    Working solution. Need cleanup and Testing
    A working solution. Now prints shortest path. Needs cleanup and to be tested more.

--- assignment5.py
+++ assignment5.py
@@ -38,7 +38,7 @@ for col in df.columns:
 #print nprime
 
 n.append(node)
-nprime.remove(node)
+#nprime.remove(node)
 
 print "The following is nPrime"
 print nprime
@@ -50,14 +50,7 @@ distX = getnum(node, 'x', df)
 distY = getnum(node, 'y', df)
 distZ = getnum(node, 'z', df)
 
-pathU = [node]
-pathV = [node]
-pathW = [node]
-pathX = [node]
-pathY = [node]
-pathZ = [node]
-
-final_path = []
+path = ["u", "v", "w", "x", "y", "z"]
 
  #Repeat
     #while 
@@ -68,8 +61,9 @@ final_path = []
     #until all of nodes in nprime
 shortest = node
 while ((len(nprime) > 0)):
+    #path = node
     minimum = 9999
-    print nprime
+    #print nprime
     for i in nprime:
         if i == 'u' and distU < minimum:
             minimum = distU
@@ -102,27 +96,111 @@ while ((len(nprime) > 0)):
             Shortdist = distY
         elif shortest == 'z':
             Shortdist = distZ
-
-    print shortest
     
     if 'u' in nprime:
-        distU = min(distU, Shortdist + getnum(shortest, 'u', df))    
+        change = Shortdist + getnum(shortest, 'u', df)
+        distU = min(distU, change)    
+        if distU == change:
+            if shortest == 'v':
+                path[0] = path[1] + 'u'
+            if shortest == 'w':
+                path[0] = path[2] + 'u'
+            if shortest == 'x':
+                path[0] = path[3] + 'u'
+            if shortest == 'y':
+                path[0] = path[4] + 'u'
+            if shortest == 'z':
+                path[0] = path[5] + 'u'
     if 'v' in nprime:
-        distV = min(distV, Shortdist + getnum(shortest, 'v', df)) 
+        change = Shortdist + getnum(shortest, 'v', df)
+        distV = min(distV, change) 
+        if distV == change:
+            if shortest == 'u':
+                path[1] = path[0] + 'v'
+            if shortest == 'w':
+                path[1] = path[2] + 'v'
+            if shortest == 'x':
+                path[1] = path[3] + 'v'
+            if shortest == 'y':
+                path[1] = path[4] + 'v'
+            if shortest == 'z':
+                path[1] = path[5] + 'v'
     if 'w' in nprime:
-        distW = min(distW, Shortdist + getnum(shortest, 'w', df)) 
+        change = Shortdist + getnum(shortest, 'w', df)
+        distW = min(distW, change) 
+        if distW == change:
+            if shortest == 'u':
+                path[2] = path[0] + 'w'
+            if shortest == 'v':
+                path[2] = path[1] + 'w'
+            if shortest == 'x':
+                path[2] = path[3] + 'w'
+            if shortest == 'y':
+                path[2] = path[4] + 'w'
+            if shortest == 'z':
+                path[2] = path[5] + 'w'
     if 'x' in nprime:
-        distX = min(distX, Shortdist + getnum(shortest, 'x', df)) 
+        change = Shortdist + getnum(shortest, 'x', df)
+        distX = min(distX, change) 
+        if distX == change:
+            if shortest == 'u':
+                path[3] = path[0] + 'x'
+            if shortest == 'v':
+                path[3] = path[1] + 'x'
+            if shortest == 'w':
+                path[3] = path[2] + 'x'
+            if shortest == 'y':
+                path[3] = path[4] + 'x'
+            if shortest == 'z':
+                path[3] = path[5] + 'x'
     if 'y' in nprime:
-        distY = min(distY, Shortdist + getnum(shortest, 'y', df)) 
+        change = Shortdist + getnum(shortest, 'y', df)
+        distY = min(distY, change) 
+        if distY == change:
+            if shortest == 'u':
+                path[4] = path[0] + 'y'
+            if shortest == 'v':
+                path[4] = path[1] + 'y'
+            if shortest == 'w':
+                path[4] = path[2] + 'y'
+            if shortest == 'x':
+                path[4] = path[3] + 'y'
+            if shortest == 'z':
+                path[4] = path[5] + 'y'
     if 'z' in nprime:
-        distZ = min(distZ, Shortdist + getnum(shortest, 'z', df)) 
+        change = Shortdist + getnum(shortest, 'z', df)
+        distZ = min(distZ, change) 
+        if distZ == change:
+            if shortest == 'u':
+                path[5] = path[0] + 'z'
+            if shortest == 'v':
+                path[5] = path[1] + 'z'
+            if shortest == 'w':
+                path[5] = path[2] + 'z'
+            if shortest == 'x':
+                path[5] = path[3] + 'z'
+            if shortest == 'y':
+                path[5] = path[4] + 'z'
 
     print "u: {}\nv: {}\nw: {}\nx: {}\ny: {}\nz: {}\n".format(distU, distV, distW, distX, distY, distZ)
 
-    n.append(shortest)
     nprime.remove(shortest)
 
+for i in path:
+    if i != node:
+        print i
 print "Shortest path tree for node {}:".format(node)
-print "Costs of least-cost paths for node {}:\nu:{}, v:{}, w:{}, x:{}, y:{}, z:{}".format(node, distU, distV, distW, distX, distY, distZ)
+if node == 'u':
+    print "{}, {}, {}, {}, {}".format(path[1], path[2], path[3], path[4], path[5])
+if node == 'v':
+    print "{}, {}, {}, {}, {}".format(path[0], path[2], path[3], path[4], path[5])
+if node == 'w':
+    print "{}, {}, {}, {}, {}".format(path[0], path[1], path[3], path[4], path[5])
+if node == 'x':
+    print "{}, {}, {}, {}, {}".format(path[0], path[1], path[2], path[4], path[5])
+if node == 'y':
+    print "{}, {}, {}, {}, {}".format(path[0], path[1], path[2], path[3], path[5])
+if node == 'z':
+    print "{}, {}, {}, {}, {}".format(path[0], path[1], path[2], path[3], path[4])
 
+print "Costs of least-cost paths for node {}:\nu:{}, v:{}, w:{}, x:{}, y:{}, z:{}".format(node, distU, distV, distW, distX, distY, distZ)
